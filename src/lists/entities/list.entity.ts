20a7fa7a48fc080fe72748aff02ba1d1bf5c7d0d
yasminbraga <yasminbragateixeira@gmail.com>
2025-10-10 13:59:27 -0300

    feat: add createdAt and updatedAt in list entity

--- src/lists/entities/list.entity.ts
+++ src/lists/entities/list.entity.ts
@@ -2,11 +2,13 @@ import { Product } from 'src/products/entities/product.entity';
 import { Recipe } from 'src/recipes/entities/recipe.entity';
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity({ name: 'lists' })
@@ -23,6 +25,18 @@ export class List {
   })
   @JoinColumn({ name: 'product_id' })
   products?: Product[];
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  updatedAt: Date;
 
   @OneToOne(() => Recipe, { nullable: true, onDelete: 'SET NULL' })
   @JoinColumn({ name: 'recipe_id' })
