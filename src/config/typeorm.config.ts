165c25472ce2e11a4639c993e940056e1352979d
yasminbraga <yasminbragateixeira@gmail.com>
2025-12-09 17:23:48 -0300

    fix: migration path in typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,12 +5,13 @@ config();
 
 const configService = new ConfigService();
 
+console.log(__dirname);
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: configService.get<string>('DB_URL'),
   synchronize: false,
   entities: [__dirname + '/**/*.entity.js'],
-  migrations: [__dirname + '/database/migrations/*.js'],
+  migrations: [__dirname + '/../database/migrations/*{.js,.ts}'],
   migrationsRun: false,
   logging: true,
   ssl:
